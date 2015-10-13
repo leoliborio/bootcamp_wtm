8170c1fcc48f9974970d291ad36deb59b9601536
Leonardo Liborio <leonardofliborio@gmail.com>
2015-10-13 20:29:59 -0300

    corrigindo zero

--- exercicios/ruby_exercicio2.rb
+++ exercicios/ruby_exercicio2.rb
@@ -2,10 +2,10 @@ puts "Por favor, digite um valor: "
 
 valor = gets.chomp.to_i
 
-if valor%2 == 0
-	puts "#{valor} é par!"
-elsif valor == 0
+if valor == 0
 	puts "Digite um valor positivo maior que zero."
+elsif valor%2 == 0
+	puts "#{valor} é par!"
 else
 	puts "#{valor} é impar!"
 end
